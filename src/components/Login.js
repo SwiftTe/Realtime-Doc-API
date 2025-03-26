727cee574cbd446bf86856c71421c31727588a2c
Rabin <masterg578585@gmail.com>
2025-03-26 12:46:07 +0545

    Create Login.js to handle auth flow

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+// Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/login', { username, password });
+      localStorage.setItem('token', response.data.token);
+      history.push('/');
+    } catch (error) {
+      alert('Login failed!');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
